ccff3d81f228a13eb115cdaa0015f636863b321a
vinerdi <vladiktaran2008@gmail.com>
2024-12-03 17:12:32 +0200

    done

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::vector<int> evenNumbers;
+
+    for (auto it = numbers.begin(); it != numbers.end(); ++it) {
+        if (*it % 2 == 0) {
+            evenNumbers.push_back(*it);
+        }
+    }
+
+    std::cout << "Even numbers: ";
+    for (auto it = evenNumbers.begin(); it != evenNumbers.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
